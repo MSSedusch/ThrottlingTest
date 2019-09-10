e9341da16155248040c78cf3ada7999d5b09153e
MSSedusch <sedusch@microsoft.com>
2019-09-10 19:55:43 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace ThrottlingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run().Wait();
+        }
+
+        public static async Task Run()
+        {
+            string subscriptionIds = GetConfigItem("subscriptionIds");
+            string clientId = GetConfigItem("clientId");
+            string clientSecret = GetConfigItem("clientSecret");
+            string tenantId = GetConfigItem("tenantId");
+
+            AzureCredentialsFactory factory = new AzureCredentialsFactory();
+            AzureCredentials azureCreds = factory.FromServicePrincipal(clientId, clientSecret, tenantId,
+                    AzureEnvironment.AzureGlobalCloud);
+            Azure.IAuthenticated azure = Azure.Configure().Authenticate(azureCreds);
+        }
+
+        private static string GetConfigItem(string key)
+        {
+            string configItem = ConfigurationManager.AppSettings[key];
+            if (String.IsNullOrEmpty(configItem))
+            {
+                throw new ConfigurationErrorsException($"Please set {key} in app.config");
+            }
+
+            return configItem;
+        }
+    }
+}
